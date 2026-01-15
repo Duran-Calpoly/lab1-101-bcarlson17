a74fac28cc3290f7b241be776e06b57de110196c
bcarlson17 <bcarls08@calpoly.edu>
2026-01-14 18:18:04 -0800

    Update lab1_4.py

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,10 @@
-pass
+def calculate_average	(num1, num2, num3)
+  avg = num1 + num2 + num3
+  avg /= 3
+  return avg
+def add_tax	(bill_total)
+  tax = bill_total / 10
+  tax = bill_total + tax
+  return tax
+def greet_user (input ('name'))
+  print ('hello' 'name')
