bf57bc9bb9efe4a403cc91c1378dd909c2560748
bcarlson17 <bcarls08@calpoly.edu>
2026-01-15 02:47:46 +0000

    lab 4 braden

--- lab1_4.py
+++ lab1_4.py
@@ -1,10 +1,15 @@
-def calculate_average	(num1, num2, num3)
+def calculate_average	(num1, num2, num3):
   avg = num1 + num2 + num3
   avg /= 3
   return avg
-def add_tax	(bill_total)
+
+def add_tax	(bill_total):
   tax = bill_total / 10
   tax = bill_total + tax
   return tax
-def greet_user (input ('name'))
-  print ('hello' 'name')
+
+def greet_user (name):
+  return "Hello " + name
+calculate_average	(1, 2, 3)
+add_tax	(50)
+greet_user (input ("Whats your name?"))
