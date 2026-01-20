5e6c5ea4c13d404fe977b1c048b6b035d309fc1c
bcarlson17 <bcarls08@calpoly.edu>
2026-01-20 04:28:46 +0000

    done

--- lab1_4.py
+++ lab1_4.py
@@ -1,1 +1,16 @@
-pass
+def calculate_average	(num1, num2, num3):
+  avg = num1 + num2 + num3
+  avg /= 3
+  return avg
+
+
+def add_tax	(bill_total):
+  tax = bill_total / 10
+  tax = bill_total + tax
+  return tax
+
+def greet_user (name):
+  return "Hello " + name
+calculate_average	(1, 2, 3)
+add_tax	(50)
+greet_user (input ("Whats your name?"))
--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,29 @@
-pass
+def check_multiple (number):
+    if number % 5 == 0 and number % 3 == 0:
+        return True
+    else:
+        return False
+    
+def check_password (input_string):
+    if input_string == "Python123":
+        return "access granted"
+    else:
+        return "access denied"
+def calculate_federal_tax (salary):
+    tax = 0
+    if salary <= 11000: 
+        tax = 0.1 * salary
+    elif salary <= 44725:
+        tax = 0.12  * salary
+    elif salary <= 95375:
+        tax = 0.22 * salary
+    elif salary > 95375:
+        tax = 0.24 * salary
+    return tax
+
+
+check_multiple (15)
+
+check_password (123)
+
+calculate_federal_tax (12000)
